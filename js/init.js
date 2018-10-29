3f456192474f2a8d2453e16b4eab3a4fd3ea3fd9
César Abraham Munguia <mbcesar@mx1.ibm.com>
2018-10-29 10:43:00 -0600

    SEKIBANKI

--- js/init.js
+++ js/init.js
@@ -7,16 +7,16 @@ var totalMoneyInMachine = 0;
 $( document ).ready(function() {
   getSodas(function(response) {
     // Parse JSON string into object
-    allSodas = JSON.parse(response).sodas;
-    currentMoneyInsideMachine = JSON.parse(response).money
-    allMoneyInMachine(currentMoneyInsideMachine);
+    allSodas = JSON.parse(response).sodas; //we parse the sodas
+    currentMoneyInsideMachine = JSON.parse(response).money //we parse the money
+    allMoneyInMachine(currentMoneyInsideMachine); //we update our local global var
     allSodas.forEach((element)=>{
-      renderSoda(element)
+      renderSoda(element) //we render each element
     })
     });
 });
 
-//Render the elements
+//Render the elements on ready
 function renderSoda(sodaData){
   $('#dataOfMachine').append(
   '<div class="row">'+
@@ -24,7 +24,7 @@ function renderSoda(sodaData){
     '<div class="card blue-grey darken-1">' +
       '<div class="card-content white-text">' +
         '<span class="card-title">' + sodaData.name + '</span>' +
-        '<a class="btn-floating halfway-fab waves-effect waves-light red" onClick=(addSoda(' + sodaData.price + '))><i class="material-icons">add</i></a>'+
+        '<a class="btn-floating halfway-fab waves-effect waves-light red" onClick=(koakuma(' + sodaData.price + '))><i class="material-icons">add</i></a>'+
         '<p>Price: $' + sodaData.price + '</p>' +
         '<p>Available: ' + sodaData.available + ' units </p>' +
         '<p>Information: $' + sodaData.description + '</p>' +
@@ -36,23 +36,29 @@ function renderSoda(sodaData){
 }
 
 //onClickEvents
-function addSoda(price){
+
+//Koakuma is best girl!
+//We retrieve the price from the card! Of course, if they click on the event
+function koakuma(price){
   $('#currentPrice').text(price)
-  getSodaByPrice(price)
+  Patchouli(price)
   console.log(currentItem)
 }
 
+//We check if the user can actually afford it
 function payForIt(){
-  canIPayIt();
+  Remilia(); //NERD!
 }
 
-function canIPayIt(){
+function Remilia(){ //I mean, don't get me wrong. Remilia is fine and all but...you know
   //whole sets of validations!!
   if(totalMoneyInMachine >= currentMoneyInMachineByUser){
     //The machine can return exchange
     if(currentMoneyInMachineByUser >= currentItem.price && currentItem.available > 0){
       //the user can afford to buy it
-      returnExchange(currentMoneyInMachineByUser - currentItem.price)
+      //-------------------------------- MOST IMPORTANT METHOD!!
+      sekibanki(currentMoneyInMachineByUser - currentItem.price) //Here, we all the method to format the change.
+      //---------------------------------
     }else{
       //the user can't afford it
       alert('Either, you dont have money or, we dont have the item :/ ')
@@ -62,19 +68,12 @@ function canIPayIt(){
     alert('We dont have enough money to give you the extra. Sorry!')
   }
 }
-
-function returnExchange(exchangeToReturn){
-  alert('exchange: ' + exchangeToReturn)
-  currentMoneyInsideMachine.map((row)=>{
-    if(exchangeToReturn >= row.value && row.ammount > 0){
-      var newExchangeToReturn = exchangeToReturn - row.value
-      console.log('$' + row.value)
-      console.log(newExchangeToReturn)
-      return true
-    }
-  })
-
+//-------------------------------- MOST IMPORTANT METHOD!!
+function sekibanki(exchangeToReturn){
+  console.log(exchangeToReturn)
+  alert(exchangeToReturn)
 }
+//-------------------------------- This is just to aid you.
 
 function changeValue (){
   currentMoneyInMachineByUser = parseInt($('#moneyByUser').val())
@@ -90,11 +89,12 @@ function allMoneyInMachine(allMoney){
 
 
 
-//GET DATA
+//GET DATA FROM LOCAL BASE
+//We need to GET the data from the local server. 
 function getSodas(callback) {   
   var xobj = new XMLHttpRequest();
       xobj.overrideMimeType("application/json");
-  xobj.open('GET', '../database/database.json', true);
+  xobj.open('POST', '../database/database.json', true);
   xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
           callback(xobj.responseText);
@@ -103,7 +103,7 @@ function getSodas(callback) {
   xobj.send(null);  
 }
 
-function getSodaByPrice(price){
+function Patchouli(price){
   //Notice that price is unique, meaning, we can use it as ID. IRL this is wrong but, you get the idea
   allSodas.forEach((row)=>{
     if(row.price === price){
